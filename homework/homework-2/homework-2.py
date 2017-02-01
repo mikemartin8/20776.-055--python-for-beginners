4e631ca04c7493d278e8f147e9b326ef3f37efcc
Mike Martin <mikem@skybox.com>
2017-02-01 14:37:24 -0800

    Homework #2 completed.

--- homework/homework-2/homework-2.py
+++ homework/homework-2/homework-2.py
@@ -10,32 +10,45 @@
 
 def AskForLetter():
     while True:
-        letter =    raw_input('Enter either one lower case or upper case letter: ')
+        letter = raw_input('Enter either one lower case or upper case letter: ')
         if letter == 'quit':
             break
         elif len(letter) == 1:
             IsVowel(letter)
         else:
             print "too many letters!"
-                
+
+## Creating the function "IsVowel(letter)". This function shall take the user
+## defined input to determine whether or not it is a vowel or not. If it is not
+## a vowel, it shall print that. If it is a vowel, it will call two additional
+## functions, "IsLowerCase(letter") & "IsUpperCase(letter)" to determine
+## whether or not the vowel is upper case or lower case.
 def IsVowel(letter):
-    if letter == ('a'):
-        status = 'True'
+    if letter == 'a' or letter == 'A' or letter == 'e' or letter == 'E' or letter == 'i' or letter == 'I' or letter == 'o'or letter == 'O' or letter == 'u'  or letter == 'U':
+        status = 'True: the input letter is a vowel.'
         print status
-    elif letter == ('e'):
-        status = 'True'
-        print status
-    elif letter == ('i'):
-        status = 'True'
-        print status
-    elif letter == ('o'):
-        status = 'True'
-        print status
-    elif letter == ('u'):
-        status = 'True'
-        print status
+        IsLowerCaseVowel(letter)
+        IsUpperCaseVowel(letter)
     else:
-        status = 'False'
+        status = 'False: your input was not a vowel.\n'
         print status
 
-IsVowel('p')
+## Creating the function "IsLowerCaseVowel(letter)" to determine if an input is
+## a lower case vowel or not.
+def IsLowerCaseVowel(letter):
+    vowels = ('a','e','i','o','u')
+    if letter.startswith(vowels) == 1:
+        print 'True: the input letter is a lower case vowel.'
+    else:
+        print 'False: the input letter is NOT a lower case vowel.'
+
+## Creating the function "IsUpperCaseVowel(letter)" to determine if an input is
+## a upper case vowel or not.
+def IsUpperCaseVowel(letter):
+    vowels = ('A','E','I','O','U')
+    if letter.startswith(vowels) == 1:
+        print 'True: the input letter is an upper case vowel.\n'
+    else:
+        print 'False: the input letter is NOT an upper case vowel.\n'
+       
+AskForLetter()
