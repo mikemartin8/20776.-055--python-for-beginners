6f9fd8efc186c4136976b8e15ddc86cf0fb05a12
Michael Martin <mimartin@mimartin-macbookpro.roam.corp.google.com>
2017-02-16 15:01:47 -0800

    Homework #4 completed. Ready for submission.

--- homework/homework-4/homework-4.py
+++ homework/homework-4/homework-4.py
@@ -20,10 +20,13 @@
 # 9)  Add 1 to each list element that is identified as a digit.
 # 10) Use 'join()' to join the new list elements together
 # 11) You must use a for loop in your code
+
 import sys
 
+# this function asks the user how many cities he/she would like to visit.
 def AskForNumberCities():
     num_cities = raw_input('How many different cities do you want to travel to?\n')
+    # checking to see if the input is a number. Program stops if input is not.
     try:
         num_cities = int(num_cities)
     except ValueError:
@@ -33,25 +36,31 @@ def AskForNumberCities():
         'ok'
     return num_cities
 
+# this function asks for the names of the cities he/she would like to visit.
 def AskForCityNames(num_cities):
     list_length =   len(range(0,num_cities+1,1))
     city_list =     []
+    # Using a for loop to check the input of each city...
+    # Using checks against each input to ensure each is in an acceptable format
     for i in range(0,num_cities,1):
         city_name =     [raw_input("Enter a new city you'd like to visit:\n")]
+        # checking if there is white space in the input
         if ' ' in city_name[0]:
-            city_list = city_list + city_name
+            if city_name[0].strip() == '':
+                print 'Your city names must contain letters'
+                sys.exit()
+            else:
+                city_list = city_list + city_name
+        # checking to see if input contains letters
         elif city_name[0].isalpha() == False:
             print 'Your city names must contain letters only.'
             sys.exit()
         else:
-            'ok'
-        if city_name[0].strip() == '':
-            print 'Your city names must contain letters'
-            sys.exit()
-        else:
-            'ok'
+            city_list = city_list + city_name
     return city_list
 
+# this function checks for duplicate entries. If there is a duplicate entry,
+# then the first entry is removed from the list
 def CheckForDuplicatesInList(city_list):
     length = len(city_list)
     for i in range(0,length,1):
@@ -62,25 +71,22 @@ def CheckForDuplicatesInList(city_list):
                 'ok'
     cities = city_list
     return cities
-    print cities
 
+# this function take a final list of cities and creates the required sentence
+# output
 def CitiesToVisitSentence(cities):
-    length =        len(cities)
-    trip_summary =  ''
-    for j in range(0,length,1):
-        if cities[j] == "":
-            'is blank'
-        else:
-            'do'"You would like to visit " + trip_summary
+    length =            len(cities)
     updated_cities =    filter(None,cities)
+    trip_summary =      ''
     preamble =          'You would like to visit '
     end =               'on your trip.'
-    trip_summary =      ''
     for k in range(0,len(updated_cities),1):
         trip_summary = trip_summary + updated_cities[k] + ' as city ' + str(k+1) + ' and '
     trip_summary = preamble + trip_summary + end
     return trip_summary
 
+# this function takes the previously generated sentence and adds one to each of
+# the numeric values
 def UpdateTripSentence(trip_sentence):
     trip_sentence_list =    trip_sentence.split()
     list_length =           len(trip_sentence_list)
@@ -93,15 +99,12 @@ def UpdateTripSentence(trip_sentence):
             'skip'
     updated_trip_summary = white_space.join(trip_sentence_list)
     return updated_trip_summary
-            
-        
-blah =      AskForNumberCities()
-city_list = AskForCityNames(blah)
-cities =    CheckForDuplicatesInList(city_list)
-sentence =  CitiesToVisitSentence(cities)
-a = UpdateTripSentence(sentence)
-print sentence
-print a
 
-#     "You would like to visit San Francisco as city 1 and San Diego as City 2
-#     and Palo Alto as city 3 on your trip."
+# running the functions
+num_cities =        AskForNumberCities()
+city_list =         AskForCityNames(num_cities)
+cities =            CheckForDuplicatesInList(city_list)
+sentence =          CitiesToVisitSentence(cities)
+sentence_update =   UpdateTripSentence(sentence)
+print sentence
+print sentence_update
